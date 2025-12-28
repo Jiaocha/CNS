70f0a8a1fe95f7a4defaa28827ac21657ad66c59
Jiaocha <w4882787@gmail.com>
2025-12-28 16:05:09 +0800

    Release v0.5.0: Network performance optimizations

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use std::sync::Arc;
 #[derive(Parser, Debug)]
 #[command(name = "cns")]
 #[command(author = "CuteBi <supercutename@gmail.com>")]
-#[command(version = "0.4.2")]
+#[command(version = "0.5.0")]
 #[command(about = "CuteBi Network Server - A high-performance network proxy server", long_about = None)]
 struct Args {
     /// JSON 配置文件路径
